3de4f2f70409d72bcdb26846c5fc2567d66f8b92
priyanshu-pj <homepc20140422@outlook.com>
2023-04-02 12:04:47 +0530

    generate invoices using weasyprint, async task to send email

--- orders/admin.py
+++ orders/admin.py
@@ -43,6 +43,11 @@ def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
     return mark_safe(f'<a href="{url}">View</a>')
 
+def order_pdf(obj):
+    url = reverse('orders:admin_order_pdf', args=[obj.id])
+    return mark_safe(f'<a href="{url}">PDF</a>')
+order_pdf.short_description = 'Invoice'
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display    =   [
@@ -57,7 +62,8 @@ class OrderAdmin(admin.ModelAdmin):
         order_payment,
         'created',
         'updated',
-        order_detail
+        order_detail,
+        order_pdf,
     ]
     list_filter     =   ['paid', 'created', 'updated']
     inlines         =   [OrderItemInline]
