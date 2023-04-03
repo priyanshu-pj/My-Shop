7bc362eb3c59b328a45b68330c8bdc2457e616e8
priyanshu-pj <homepc20140422@outlook.com>
2023-04-03 08:43:13 +0530

    coupons discounts to stripe payments

--- payment/views.py
+++ payment/views.py
@@ -37,6 +37,16 @@ def payment_process(request):
                 },
                 'quantity': item.quantity,
             })
+        # stripe coupon
+        if order.coupon:
+            stripe_coupon = stripe.Coupon.create(
+                    name=order.coupon.code,
+                    percent_off=order.discount,
+                    duration='once'
+                    )
+            session_data['discounts'] = [{
+                'coupon': stripe_coupon.id
+            }]
         # stripe checkout session
         session = stripe.checkout.Session.create(**session_data)
         return redirect(session.url, code=303)
